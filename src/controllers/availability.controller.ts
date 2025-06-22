e08c182d01484e16c78af65cbdf35ae3224a069f
ahmed-said7 <engahmedsaid125@gmail.com>
2025-06-22 03:06:58 +0300

    booking updates

--- src/controllers/availability.controller.ts
+++ src/controllers/availability.controller.ts
@@ -1,19 +1,19 @@
 import {
   Controller,
   Get,
-  Post,
+  // Post,
   Body,
   Req,
   UseGuards,
-  Param,
+  // Param,
   Patch,
   Query,
-  Delete,
+  // Delete,
 } from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../core/jwt-auth-guard/jwt-auth.guard';
 import { AvailabilityService } from '../services/availability.service';
-import { CreateAvailabilityDto } from '../dtos/create_Availability.dto';
+// import { CreateAvailabilityDto } from '../dtos/create_Availability.dto';
 import { UpdateAvailability } from '../dtos/update_Avalability';
 import { ResponseDto } from '../dtos/response.dto';
 import { RoleGuard } from '../core/role/role.guard';
@@ -25,40 +25,38 @@ import { All_Role } from '../types/enum';
 export class AvailabilityController {
   constructor(private readonly availabilityService: AvailabilityService) {}
 
-  @Post()
-  @UseGuards(JwtAuthGuard, RoleGuard)
-  @Role([All_Role.Doctor])
-  async createAvailability(
-    @Body() createAvailabilityDto: CreateAvailabilityDto,
-    @Req() req: any,
-    @Query('timezone') timezone: string,
-  ) {
-    if (!timezone) {
-      timezone = 'Africa/Cairo';
-    }
-    try {
-      console.log('payyyy,', req.user);
-      createAvailabilityDto.doctorId = req.user.userId;
-      const response = await this.availabilityService.createAvailability(
-        createAvailabilityDto,
-        timezone,
-      );
-      return ResponseDto.ok(response);
-    } catch (err) {
-      return ResponseDto.throwBadRequest(err.message, err);
-    }
-  }
+  // @Post()
+  // @UseGuards(JwtAuthGuard, RoleGuard)
+  // @Role([All_Role.Doctor])
+  // async createAvailability(
+  //   @Body() createAvailabilityDto: CreateAvailabilityDto,
+  //   @Req() req: any,
+  //   @Query('timezone') timezone: string,
+  // ) {
+  //   if (!timezone) {
+  //     timezone = 'Africa/Cairo';
+  //   }
+  //   try {
+  //     console.log('payyyy,', req.user);
+  //     createAvailabilityDto.doctorId = req.user.userId;
+  //     const response = await this.availabilityService.createAvailability(
+  //       createAvailabilityDto,
+  //       timezone,
+  //     );
+  //     return ResponseDto.ok(response);
+  //   } catch (err) {
+  //     return ResponseDto.throwBadRequest(err.message, err);
+  //   }
+  // }
 
-  @Get(':doctorId')
+  @Get()
   @UseGuards(JwtAuthGuard)
-  async getAvailability(
-    @Param('doctorId') doctorId: string,
-    @Query('timezone') timezone: string,
-  ) {
+  async getAvailability(@Query('timezone') timezone: string, @Req() req: any) {
     try {
       if (!timezone) {
         timezone = 'Africa/Cairo';
       }
+      const doctorId = req.user.userId;
       const response = await this.availabilityService.getAvailability(
         doctorId,
         timezone,
@@ -93,17 +91,17 @@ export class AvailabilityController {
     }
   }
 
-  @Delete()
-  @UseGuards(JwtAuthGuard, RoleGuard)
-  @Role([All_Role.Doctor])
-  async deleteAvaialability(@Req() req: any) {
-    try {
-      const doctorId = req.user.userId;
-      const response =
-        await this.availabilityService.deleteAvailability(doctorId);
-      return ResponseDto.ok(response);
-    } catch (err) {
-      return ResponseDto.throwBadRequest(err.message, err);
-    }
-  }
+  // @Delete()
+  // @UseGuards(JwtAuthGuard, RoleGuard)
+  // @Role([All_Role.Doctor])
+  // async deleteAvaialability(@Req() req: any) {
+  //   try {
+  //     const doctorId = req.user.userId;
+  //     const response =
+  //       await this.availabilityService.deleteAvailability(doctorId);
+  //     return ResponseDto.ok(response);
+  //   } catch (err) {
+  //     return ResponseDto.throwBadRequest(err.message, err);
+  //   }
+  // }
 }
